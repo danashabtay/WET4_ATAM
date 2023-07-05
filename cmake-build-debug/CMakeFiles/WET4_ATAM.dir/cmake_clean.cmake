9eb3fabf61789705255bc28110d13ce633f8f079
danashabtay <99danashabtay@gmail.com>
2023-07-05 17:08:51 +0300

    files

---
+++ cmake-build-debug/CMakeFiles/WET4_ATAM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WET4_ATAM.dir/main.cpp.obj"
+  "WET4_ATAM.exe"
+  "WET4_ATAM.exe.manifest"
+  "WET4_ATAM.pdb"
+  "libWET4_ATAM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WET4_ATAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/WET4_ATAM.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WET4_ATAM.
+# This may be replaced when dependencies are built.
